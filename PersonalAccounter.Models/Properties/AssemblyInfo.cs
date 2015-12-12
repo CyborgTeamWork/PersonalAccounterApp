d8769ac938e6752722d5cfbc8a6ac39fb7340ce4
darkyto <iliev.nick007@gmail.com>
2015-12-12 15:06:02 +0200

    Basic MVVM solution structure. Added basic Expense.cs and ExpenseManager.cs models. Added Seed method. Added NotifyPropertyChanged method and ObservableCollections

--- PersonalAccounter.Models/Properties/AssemblyInfo.cs
+++ PersonalAccounter.Models/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PersonalAccounter.Model")]
+[assembly: AssemblyTitle("PersonalAccounter.Models")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PersonalAccounter.Model")]
+[assembly: AssemblyProduct("PersonalAccounter.Models")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
