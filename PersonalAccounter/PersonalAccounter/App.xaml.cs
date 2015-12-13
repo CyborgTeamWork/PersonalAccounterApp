978f0a99f02507f45adc1f374a9072b0d0e9c2ab
darkyto <iliev.nick007@gmail.com>
2015-12-13 23:24:40 +0200

    added SplitView menu with several test pages options and a Back functionality

--- PersonalAccounter/PersonalAccounter/App.xaml.cs
+++ PersonalAccounter/PersonalAccounter/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PersonalAccounter.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -75,7 +76,7 @@ namespace PersonalAccounter
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                rootFrame.Navigate(typeof(AppShell), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
