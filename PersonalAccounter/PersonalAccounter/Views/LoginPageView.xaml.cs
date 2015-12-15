64dff839ed13f2d16fa203dab0633e930bc37f93
darkyto <iliev.nick007@gmail.com>
2015-12-15 22:06:47 +0200

    corrected aligment in login-form

---
+++ PersonalAccounter/PersonalAccounter/Views/LoginPageView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace PersonalAccounter.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPageView : Page
+    {
+        public LoginPageView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Toggle_Click_Login(object sender, RoutedEventArgs e)
+        {
+            if (!logincontrol1.IsOpen)
+            {
+                parent.IsEnabled = false;
+                this.Opacity = .4;
+                container.IsEnabled = true;
+                logincontrol1.IsOpen = true;
+                pop.Width = Window.Current.Bounds.Width;
+            }
+            else
+            {
+                logincontrol1.IsOpen = false;
+                this.Opacity = 1.0;
+                parent.IsEnabled = true;
+            }
+        }
+    }
+}
