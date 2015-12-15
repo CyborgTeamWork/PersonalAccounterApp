f9504c1e55a879fbe43d0ef4cb5a8ae08e30d7be
darkyto <iliev.nick007@gmail.com>
2015-12-15 21:32:45 +0200

    some style refactoring

--- PersonalAccounter/PersonalAccounter/App.xaml.cs
+++ PersonalAccounter/PersonalAccounter/App.xaml.cs
@@ -76,7 +76,7 @@ namespace PersonalAccounter
             {
                 // When the navigation stack isn't restored, navigate to the first page
                 // suppressing the initial entrance animation.
-                shell.AppFrame.Navigate(typeof(LandingPage), e.Arguments, new Windows.UI.Xaml.Media.Animation.SuppressNavigationTransitionInfo());
+                shell.AppFrame.Navigate(typeof(LoginPageView), e.Arguments, new Windows.UI.Xaml.Media.Animation.SuppressNavigationTransitionInfo());
             }
 
             // Ensure the current window is active
