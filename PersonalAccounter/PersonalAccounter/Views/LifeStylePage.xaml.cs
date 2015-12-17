8701f8d886c62a18204554556e577620b20108a1
darkyto <iliev.nick007@gmail.com>
2015-12-17 21:03:07 +0200

    added HouseHoldPage.xaml view and view model

--- PersonalAccounter/PersonalAccounter/Views/LifeStylePage.xaml.cs
+++ PersonalAccounter/PersonalAccounter/Views/LifeStylePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using PersonalAccounter.ViewModels;
+using PersonalAccounter.ViewModels.PersonalAccounter.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -29,13 +30,13 @@ namespace PersonalAccounter.Views
         public LifeStylePage()
         {
             this.InitializeComponent();
-            var contentViewModel = new HouseHoldContentViewModel();
+            var contentViewModel = new LifestyleContentViewModel();
 
-            contentViewModel.HouseHoldExpenses = new List<HouseHoldViewModel>()
+            contentViewModel.LifeStyleExpenses = new List<LifestyleViewModel>()
             {
-                new HouseHoldViewModel("HOuse Starwars Premiere", "http://i.dailymail.co.uk/i/pix/2008/12/12/article-1094102-02C8957A000005DC-866_468x435.jpg", 40, "The Force is awakening"),
-                new HouseHoldViewModel("House Friday Night", "http://www.preapps.com/blog/wp-content/uploads/2015/11/tumblr_static_party-music-hd-wallpaper-1920x1200-38501.jpg", 100, "TIME TO PARTYYYY"),
-                new HouseHoldViewModel("HOuse Monthly Ciggaretes", "http://www.positivechoices.com/sites/www.positivechoices.com/files/images/cigarette%20pack.jpeg", 150, "Bad Bad bad!")
+                new LifestyleViewModel("HOuse Starwars Premiere", "http://i.dailymail.co.uk/i/pix/2008/12/12/article-1094102-02C8957A000005DC-866_468x435.jpg", 40, "The Force is awakening"),
+                new LifestyleViewModel("House Friday Night", "http://www.preapps.com/blog/wp-content/uploads/2015/11/tumblr_static_party-music-hd-wallpaper-1920x1200-38501.jpg", 100, "TIME TO PARTYYYY"),
+                new LifestyleViewModel("HOuse Monthly Ciggaretes", "http://www.positivechoices.com/sites/www.positivechoices.com/files/images/cigarette%20pack.jpeg", 150, "Bad Bad bad!")
             };
 
             this.DataContext = new MainPageViewModel(contentViewModel);
