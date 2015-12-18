f260a26231604b1368bf4e66fb878d599b19814e
darkyto <iliev.nick007@gmail.com>
2015-12-18 17:02:06 +0200

    BudjetContentViewModel.cs BudjetPage.xaml BudjetViewModel.cs + some styles changes

---
+++ PersonalAccounter/PersonalAccounter/ViewModels/BudjetContentViewModel.cs
@@ -0,0 +1,70 @@
+﻿namespace PersonalAccounter.ViewModels
+{
+    using Extensions;
+    using Helpers;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+
+    public class BudjetContentViewModel : ViewModelBase, IContentViewModel
+    {
+        public ObservableCollection<BudjetViewModel> monthlyBudjets;
+        private ICommand saveCommand;
+        private ICommand deleteCommand;
+        private ICommand editCommand;
+
+        public IEnumerable<BudjetViewModel> MonthlyBudjets
+        {
+            get
+            {
+                if (this.monthlyBudjets == null)
+                {
+                    this.monthlyBudjets = new ObservableCollection<BudjetViewModel>();
+                }
+
+                return this.monthlyBudjets;
+            }
+            set
+            {
+                if (this.monthlyBudjets == null)
+                {
+                    this.monthlyBudjets = new ObservableCollection<BudjetViewModel>();
+                }
+
+                this.monthlyBudjets.Clear();
+                value.ForEach(this.monthlyBudjets.Add);
+            }
+        }
+
+        public ICommand Save
+        {
+            get
+            {
+                if (this.saveCommand == null)
+                {
+                    this.saveCommand = new DelegateCommand<BudjetViewModel>((newBudjet) =>
+                    {
+                        this.monthlyBudjets.Add(new BudjetViewModel(newBudjet));
+                    });
+                }
+                return this.saveCommand;
+            }
+        }
+
+        public ICommand Delete
+        {
+            get
+            {
+                if (this.deleteCommand == null)
+                {
+                    this.deleteCommand = new DelegateCommand<BudjetViewModel>((budjet) =>
+                    {
+                        this.monthlyBudjets.Remove(budjet);
+                    });
+                }
+                return this.deleteCommand;
+            }
+        }
+
+    }
+}
---
+++ PersonalAccounter/PersonalAccounter/ViewModels/BudjetViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounter.ViewModels
+{
+    public class BudjetViewModel : ViewModelBase
+    {
+        public BudjetViewModel()
+                : this(0, 0, 0, 0)
+            {
+        }
+
+        public BudjetViewModel(BudjetViewModel newBudjet)
+                : this(newBudjet.TotalBudjet, newBudjet.HomeExpecations, newBudjet.LifeExpecations, newBudjet.UnexpectedExpecations)
+            {
+
+        }
+
+        public BudjetViewModel(int total, int home, int life, int unexpected)
+        {
+            this.TotalBudjet = total;
+            this.HomeExpecations = home;
+            this.LifeExpecations = life;
+            this.UnexpectedExpecations = unexpected;
+        }
+
+
+        public int TotalBudjet { get; set; }
+
+        public int HomeExpecations { get; set; }
+
+        public int LifeExpecations { get; set; }
+
+        public int UnexpectedExpecations { get; set; }
+
+        public bool Equals(BudjetViewModel obj)
+        {
+            return this.TotalBudjet == obj.TotalBudjet &&
+              this.HomeExpecations == obj.HomeExpecations &&
+              this.LifeExpecations == obj.LifeExpecations &&
+              this.UnexpectedExpecations == obj.UnexpectedExpecations;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as BudjetViewModel;
+
+            if (other == null)
+            {
+                return false;
+            }
+            return this.Equals(other);
+        }
+    }
+}
